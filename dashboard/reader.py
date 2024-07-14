e3c7a5e7a624ddd7134ca1eee6757623f55b5416
spezialissimo <davide.speziali@proton.me>
2024-07-14 22:41:21 +0200

    Test using

--- dashboard/reader.py
+++ dashboard/reader.py
@@ -4,21 +4,24 @@ import pytz
 from time import sleep
 import json
 from flask import Flask, jsonify, render_template, request
-import requests
+import random
 from threading import Thread
 
 app = Flask(__name__)
 
 host = '127.0.0.1'
-port = 5000
+port = 5001
 last_received = ''
 
 tz = pytz.timezone("Europe/Rome")
 
 SERIAL_PORT = '/dev/ttyACM0'
 SERIAL_BAUDRATE = 9600
+DEMO = True
 
-ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
+ser = ""
+if ( DEMO != True ):
+    serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
 
 @app.route('/')
 def index():
@@ -26,14 +29,24 @@ def index():
 
 @app.route('/getLastReadings', methods=['GET'])
 def get_data():
-    print(last_received)
-    moisture_values = {}
-    if( last_received != ""):
-        try:
-            moisture_values = json.loads(last_received)
-            print(json.dumps(moisture_values, indent=4))
-        except:
-            print("Error parsing json")
+    if( DEMO ):
+        moisture_values = {
+            "ms_10_10": random.randint(0, 100),
+            "ms_10_30": random.randint(0, 100),
+            "ms_20_10": random.randint(0, 100),
+            "ms_20_30": random.randint(0, 100),
+            "ms_30_10": random.randint(0, 100),
+            "ms_30_30": random.randint(0, 100)
+        }
+    else:
+        print(last_received)
+        moisture_values = {}
+        if( last_received != ""):
+            try:
+                moisture_values = json.loads(last_received)
+                print(json.dumps(moisture_values, indent=4))
+            except:
+                print("Error parsing json")
     return jsonify(moisture_values)
 
 def receiving(ser):
@@ -55,6 +68,7 @@ def start_flask(host, port):
     app.run(host=host, port=port, debug=False)
 
 if __name__ == '__main__':
-    Thread(target=receiving, args=(ser,)).start()
+    if( DEMO != True ):
+        Thread(target=receiving, args=(ser,)).start()
     Thread(target=start_flask, args = (host, port)).start()
 
