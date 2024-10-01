a8c0313c56c6d532af02c00d6861816761214519
Spezialissimo <davide.speziali@protonmail.com>
2024-10-01 16:01:45 +0200

    Better moisture curve

--- dashboard/hardware/hardware.py
+++ dashboard/hardware/hardware.py
@@ -61,6 +61,28 @@ class Hardware:
         self.left_sprinkler_open = not self.left_sprinkler_open
 
     def __update_sensor_values(self):
+        self.__FLACTUATE_MIN_INCREASE = 90
+        self.__FLACTUATE_MAX_INCREASE = 99
+        self.__FLACTUATE_MIN_DECREASE = 1
+        self.__FLACTUATE_MAX_DECREASE = 10
+        self.__THRESHOLD_RAPID_INCREASE = 4
+        self.__THRESHOLD_RAPID_DECREASE = 3.5
+        self.__DECREASE_FACTOR_RAPID = 6
+        self.__DECREASE_FACTOR_SLOW = 2
+        self.__INCREASE_FACTOR_RAPID = 8
+        self.__INCREASE_FACTOR_SLOW = 3
+
+        def new_value_pump_off(current_value, decrease_factor):
+            if current_value < self.__FLACTUATE_MAX_DECREASE:
+                return fluctuate(self.__FLACTUATE_MIN_DECREASE, self.__FLACTUATE_MAX_DECREASE, current_value, 3)
+            else:
+                return max(self.__FLACTUATE_MIN_DECREASE, min(99, current_value - (random.random() * decrease_factor)))
+        def new_value_pump_on(current_value, decrease_factor):
+            if current_value > self.__FLACTUATE_MIN_INCREASE:
+                return fluctuate(self.__FLACTUATE_MIN_INCREASE, self.__FLACTUATE_MAX_INCREASE, current_value, 3)
+            else:
+                return max(1, min(self.__FLACTUATE_MAX_INCREASE, current_value + (random.random() * decrease_factor)))
+
         def fluctuate(min_val, max_val, current_value, magnitude):
             """Fluttua il valore attuale tra il minimo e il massimo, con un'oscillazione controllata."""
             delta = (random.random() - 0.5) * magnitude  # Oscillazione casuale
@@ -70,73 +92,34 @@ class Hardware:
 
             elapsed_time = time.time() - self.initial_pump_time
 
-            # Imposta la durata della fase di diminuzione rapida (ad esempio 10 secondi)
-            rapid_decrease_duration = 3.5
+            rapid_decrease_duration = self.__THRESHOLD_RAPID_DECREASE
 
-            # Diminuzione rapida all'inizio
             if elapsed_time < rapid_decrease_duration:
-                decrease_factor = 4  # Diminuzione più rapida iniziale
-                self.sensor_values["10_5"] = max(1, min(99, self.sensor_values["10_5"] - (random.random() * decrease_factor)))
-                self.sensor_values["10_15"] = max(1, min(99, self.sensor_values["10_15"] - (random.random() * decrease_factor)))
-                self.sensor_values["10_25"] = max(1, min(99, self.sensor_values["10_25"] - (random.random() * decrease_factor)))
-
-                if self.left_sprinkler_open:
-                    self.sensor_values["30_5"] = max(1, min(99, self.sensor_values["30_5"] - (random.random() * decrease_factor)))
-                    self.sensor_values["30_15"] = max(1, min(99, self.sensor_values["30_15"] - (random.random() * decrease_factor)))
-                    self.sensor_values["30_25"] = max(1, min(99, self.sensor_values["30_25"] - (random.random() * decrease_factor)))
+                decrease_factor = self.__DECREASE_FACTOR_RAPID
             else:
-                # Dopo i 10 secondi, diminuire lentamente
-                slow_decrease_factor = 2  # Diminuzione più lenta per i valori
-                self.sensor_values["10_5"] = max(1, min(99, self.sensor_values["10_5"] - (random.random() * slow_decrease_factor)))
-                self.sensor_values["10_15"] = max(1, min(99, self.sensor_values["10_15"] - (random.random() * slow_decrease_factor)))
-                self.sensor_values["10_25"] = max(1, min(99, self.sensor_values["10_25"] - (random.random() * slow_decrease_factor)))
+                decrease_factor = self.__DECREASE_FACTOR_SLOW
 
-                if self.left_sprinkler_open:
-                    self.sensor_values["30_5"] = max(1, min(99, self.sensor_values["30_5"] - (random.random() * slow_decrease_factor)))
-                    self.sensor_values["30_15"] = max(1, min(99, self.sensor_values["30_15"] - (random.random() * slow_decrease_factor)))
-                    self.sensor_values["30_25"] = max(1, min(99, self.sensor_values["30_25"] - (random.random() * slow_decrease_factor)))
-                else:
-                    # Se il left_sprinkler è chiuso, fluttuano verso il basso tra 1 e 5
-                    self.sensor_values["30_5"] = fluctuate(1, 5, self.sensor_values["30_5"], 1)
-                    self.sensor_values["30_15"] = fluctuate(1, 5, self.sensor_values["30_15"], 1)
-                    self.sensor_values["30_25"] = fluctuate(1, 5, self.sensor_values["30_25"], 1)
+            for sensor in self.sensor_values:
+                self.sensor_values[sensor] = new_value_pump_off(self.sensor_values[sensor], decrease_factor)
 
         else:
             elapsed_time = time.time() - self.initial_pump_time
 
-            # Imposta la durata della fase di aumento rapido (ad esempio 10 secondi)
-            rapid_increase_duration = 4
+            rapid_increase_duration = self.__THRESHOLD_RAPID_INCREASE
 
-            # Incremento massimo consentito durante il periodo rapido
             if elapsed_time < rapid_increase_duration:
-                increment_factor = 4  # Incremento più rapido iniziale
-                # Aumento dei valori, evitando di superare 94
-                self.sensor_values["10_5"] = max(1, min(94, self.sensor_values["10_5"] + (random.random() * increment_factor)))
-                self.sensor_values["10_15"] = max(1, min(94, self.sensor_values["10_15"] + (random.random() * increment_factor)))
-                self.sensor_values["10_25"] = max(1, min(94, self.sensor_values["10_25"] + (random.random() * increment_factor)))
-
-                if self.left_sprinkler_open:
-                    self.sensor_values["30_5"] = max(1, min(94, self.sensor_values["30_5"] + (random.random() * increment_factor)))
-                    self.sensor_values["30_15"] = max(1, min(94, self.sensor_values["30_15"] + (random.random() * increment_factor)))
-                    self.sensor_values["30_25"] = max(1, min(94, self.sensor_values["30_25"] + (random.random() * increment_factor)))
+                increment_factor = self.__INCREASE_FACTOR_RAPID
             else:
-                # Dopo i 10 secondi, aumentare lentamente verso 95
-                # Definiamo un passo di aumento più lento
-                slow_increment_factor = 2  # Incremento più lento per i valori
-                self.sensor_values["10_5"] = max(1, min(99, self.sensor_values["10_5"] + (random.random() * slow_increment_factor)))
-                self.sensor_values["10_15"] = max(1, min(99, self.sensor_values["10_15"] + (random.random() * slow_increment_factor)))
-                self.sensor_values["10_25"] = max(1, min(99, self.sensor_values["10_25"] + (random.random() * slow_increment_factor)))
+                increment_factor = self.__INCREASE_FACTOR_SLOW
 
+            for sensor in self.sensor_values:
                 if self.left_sprinkler_open:
-                    self.sensor_values["30_5"] = max(1, min(99, self.sensor_values["30_5"] + (random.random() * slow_increment_factor)))
-                    self.sensor_values["30_15"] = max(1, min(99, self.sensor_values["30_15"] + (random.random() * slow_increment_factor)))
-                    self.sensor_values["30_25"] = max(1, min(99, self.sensor_values["30_25"] + (random.random() * slow_increment_factor)))
+                    self.sensor_values[sensor] = new_value_pump_on(self.sensor_values[sensor], increment_factor)
                 else:
-                    self.sensor_values["30_5"] = fluctuate(1, 5, self.sensor_values["30_5"], 1)
-                    self.sensor_values["30_15"] = fluctuate(1, 5, self.sensor_values["30_15"], 1)
-                    self.sensor_values["30_25"] = fluctuate(1, 5, self.sensor_values["30_25"], 1)
-
-
+                    if(sensor == "30_5" or sensor == "30_15" or sensor == "30_25"):
+                        self.sensor_values[sensor] = new_value_pump_off(self.sensor_values[sensor], self.__DECREASE_FACTOR_RAPID)
+                    else:
+                        self.sensor_values[sensor] = new_value_pump_on(self.sensor_values[sensor], increment_factor)
 
 
 
